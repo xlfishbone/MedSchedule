5bec1a10df2a6abaa84797de911c1e750cd6b35e
Josh Allen <beefzilla@gmail.com>
2020-02-10 23:58:29 -0500

    added python flask app

---
+++ src/Api/flask/app.py
@@ -0,0 +1,22 @@
+import base64
+from hashlib import sha256
+
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/hash/<string:ptext>', methods=['GET'])
+def hello_world(ptext):
+    return jsonify(hashCode=hash_text(ptext)) 
+
+
+def hash_text(plain_text):
+    hashedBytes = sha256(plain_text.encode()).digest()    
+    encoded = base64.b64encode(hashedBytes)
+    return encoded
+
+
+if __name__ == '__main__':
+    app.run()
